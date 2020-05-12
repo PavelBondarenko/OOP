bbe721831a4434dea5829e1b42ca3b0058443473
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-05-12 19:43:42 +0300

    tictac

--- tictactoe/test2.php
+++ tictactoe/test2.php
@@ -9,6 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <style>
     .tictac td {
         border: 1px solid black;
